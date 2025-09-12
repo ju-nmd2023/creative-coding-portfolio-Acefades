d161b23cb0cfaebf275949c236285edfb095afe1
Acefades <144893637+Acefades@users.noreply.github.com>
2025-09-12 10:49:03 +0200

    made them more wobbly, random colors, background changes etc

--- experiments/square2.js
+++ experiments/square2.js
@@ -1,14 +1,14 @@
 function setup() {
   createCanvas(600, 600);
-  frameRate(3);
-}
+}  
 
 const size = 100;
 const layers = 10;
+const gap = 20;
 
 function getRandomValue(pos, variance) {
   return pos + map(Math.random(), 0, 1, -variance, variance);
-}                      
+}
 
 function drawLayers(x, y, size, layers) {
   const variance = size / 20;
@@ -22,11 +22,14 @@ function drawLayers(x, y, size, layers) {
     }
 
     const s = (size / layers) * i;
-    const half = s / 2;
+    const half = s / 4;
   
-  
-    for (let j = 0; j < 3; j++) {
-      beginShape();
+  strokeWeight(random(2,8));
+  stroke(random(200,255),random(200,255),random(0,255)); 
+    
+    for (let j = 0; j < 3; j++) { 
+      fill (random(255),random(255),random(255),20);  // wobbly squares      
+      beginShape(); //wobbly squares
       vertex(
         getRandomValue(x - half, variance),
         getRandomValue(y - half, variance)
@@ -34,7 +37,7 @@ function drawLayers(x, y, size, layers) {
       vertex(
         getRandomValue(x + half, variance),
         getRandomValue(y - half, variance)
-      );
+      );            
       vertex(
         getRandomValue(x + half, variance),
         getRandomValue(y + half, variance)
@@ -45,18 +48,23 @@ function drawLayers(x, y, size, layers) {
       );
       endShape(CLOSE);
     }
-  }
+  } 
 }
 
 function draw() {
-  background(random(255), random(255), random(255));
+  background(random(255),random(255),random(255));
 
-  for (let y = 0; y < 10; y++) {
-    for (let x = 0; x < 10; x++) {
-      drawLayers(size / 2 + x * size, size / 2 + y * size, size, layers);
+  for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
+      drawLayers(
+        size / 2 + x * (size + gap),
+        size / 2 + y * (size + gap),
+        size,
+        layers
+      );
+
     }
-  }
-
-
+  }       
+   
+  noLoop();
 }
- 
