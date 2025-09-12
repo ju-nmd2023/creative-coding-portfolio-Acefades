912d20e9de9691a8982effbfd597ec81af773496
Acefades <144893637+Acefades@users.noreply.github.com>
2025-09-12 11:50:05 +0200

    Added more waves, with colors and make them random

--- experiments/line art.js
+++ experiments/line art.js
@@ -6,16 +6,23 @@ function draw () {
     background (255,255,255);
     const originalY = 300; // center of canvas
     const divider = 50;
-    noiseSeed(50);
-    beginShape();
-  for (let x =0; x < 600; x++) {   // from width 0 to 600 is drawn
-    //const y = originalY + Math.random() * 100;
-    const y = originalY + noise(x / divider) * 100;
-    
-  vertex(x,y);
+    // draw more waves
+    for (let i = 0; i < 5;i++) {
+      noiseSeed(i*100);
+      stroke(random(255), random(255),random(255));
+      noFill();
+      beginShape();
+      for (let x = 0; x < 600; x++) {
+        // from width 0 to 600 is drawn
+        //const y = originalY + Math.random() * 100;
+        const y = originalY + noise(x / divider) * 100;
+
+        vertex(x, y);
+      }
+      endShape();
 
 
     }
-    endShape();
-    noLoop();
+    
+    noLoop();      
 }
